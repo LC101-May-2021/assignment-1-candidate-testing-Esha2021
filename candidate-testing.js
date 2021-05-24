fa8452f9c4d01649a9d405509f3d609468183faf
Esha2022 <>
2021-05-24 16:30:46 +0000

    score modified in Candidate test

--- candidate-testing.js
+++ candidate-testing.js
@@ -23,25 +23,26 @@ questions=[1,2,3,4,5];
 correctAnswer=['Sally Ride','true','40','Trajectory',3];
 canditateAnswers=[1,2,3,4,5];
 
-questions[0]= "1...Who was the first American woman in space? ";
+questions[0]= " 1) Who was the first American woman in space? ";
 
-questions[1]="2...True or false: 5 kilometer == 5000 meters?";
-questions[2]="3...(5 + 3)/2 * 10 = ?";
-questions[3]="4...Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?";
-questions[4]="5..What is the minimum crew size for the ISS?";	
-console.log( questions[0]);
+
+questions[1]="2) True or false: 5 kilometer == 5000 meters?";
+questions[2]="3) (5 + 3)/2 * 10 = ?";
+questions[3]="4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?";
+questions[4]="5) What is the minimum crew size for the ISS?";	
+console.log("\n"+ questions[0]);
 canditateAnswers[0]=input.question("your Answer:");
 console.log("correctAnswer :"+ correctAnswer[0]);
-console.log( questions[1]);
+console.log("\n "+questions[1]);
 canditateAnswers[1]=input.question("your Answer:");
 console.log("correctAnswer :" + correctAnswer[1]);
-console.log( questions[2]);
+console.log("\n "+ questions[2]);
 canditateAnswers[2]=input.question("your Answer:");
 console.log("correctAnswer :" +correctAnswer[2]);
-console.log( questions[3]);
+console.log("\n "+ questions[3]);
 canditateAnswers[3]=input.question("your Answer:");
 console.log("correctAnswer :"+ correctAnswer[3]);
-console.log( questions[4]);
+console.log( "\n "+questions[4]);
 canditateAnswers[4]=input.question("your Answer:");
 console.log("correctAnswer :" +correctAnswer[4]);
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
@@ -51,39 +52,37 @@ console.log("correctAnswer :" +correctAnswer[4]);
 
 function gradeQuiz(candidateAnswers) {
   let grade=0;
-if(correctAnswer[0]===canditateAnswers[0])
+for(let i=0; i<=questions.length;i++)
+{
+  if(canditateAnswers[i]===correctAnswer[i])
 {
   grade=grade+1;
-
-}
-if(correctAnswer[1]===canditateAnswers[1])
-{
-  grade=grade+1;
-
-}
-if(correctAnswer[2]===canditateAnswers[2])
-{
-  grade=grade+1;
-
-}
-if(correctAnswer[3]===canditateAnswers[3])
-{
-  grade=grade+1;
-
-}
-if(correctAnswer[4]===canditateAnswers[4])
-{
-  grade=grade+1;
-
-}else{
-  grade=0;
+  //console.log(grade);
 }
 
+ }
+  console.log(grade);
   
+  let gradescore=grade/questions.length*100;
+  console.log(`>>> overall grade: ${gradescore}% (${grade} of ${questions.length}responses correct ) <<<`);
+  if(gradescore>=80)
+  {
+    console.log(">>> Staus:PASSED <<<");
+
+  }
+  else{
+    console.log(">>> Status:FAILED  <<<");
+  }
+
+
+
+// else
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
 
-  //let grade;
+  //let grade;Number of Correct Answers) / (Number of Quiz Questions) * 100
+
+//Example Output
   
 
   return grade;
