bf3a48a62d5e6bd76b7cb01b4be2dd5c752099d4
Esha2022 <>
2021-05-24 22:57:16 +0000

    greetings added

--- candidate-testing.js
+++ candidate-testing.js
@@ -92,6 +92,7 @@ function runProgram() {
   askForName();
   //candidateName=input.question(" Enter Candidate Name:");
   // TODO 1.1c: Ask for candidate's name //
+  console.log("Welgome!");
   
   askQuestion();
   gradeQuiz(this.candidateAnswers);
