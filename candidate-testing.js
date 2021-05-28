ecd47b3dce8c8abef9b039f972b0f2eb0aeb893c
Esha2022 <>
2021-05-28 23:54:40 +0000

    canditaQuestion modified

--- candidate-testing.js
+++ candidate-testing.js
@@ -7,10 +7,10 @@ const input = require('readline-sync');
 
  let candidateName= "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question= "Who was the first American woman in space?";
+let question= "Who was the first American woman in space? ";
 let correctAnswer="Sally Ride";
 let candidateAnswer=" ";
-let questions=["1) Who was the first American woman in space?","2) True or false: 5 kilometer == 5000 meters?","3) (5 + 3)/2 * 10 = ?","4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","5) What is the minimum crew size for the ISS?" ];
+let questions=["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? " ];
 let correctAnswers=['Sally Ride','true','40','Trajectory','3'];;
 let candidateAnswers=[ ];
 
@@ -48,7 +48,7 @@ for(let i=0; i<questions.length;i++)
     console.log("Candidate Name:  " +candidateName);
     for (let i=0;i<questions.length;i++)
     {
-      console.log(questions[i]);
+      console.log(`${i+1}) ${questions[i]}`);
       console.log("Your Answer:  " + candidateAnswers[i]);
       console.log("Correct Answer:  " + correctAnswers[i]+"\n");
     
